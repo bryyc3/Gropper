65ad3e5db0016cdbac87284446fa75aa6dd5db99
Bryce King <bryceeking5@gmail.com>
2025-04-10 20:47:26 -0400

    Initial Commit

---
+++ Gropper/GropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GropperApp.swift
+//  Gropper
+//
+//  Created by Bryce King on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
